34ed0321f4afece52826ddb114f4c4f8007e67be
lorenzo lulli <lullilore@gmail.com>
2020-02-17 18:26:23 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt("qual è il tuo nome?")
+var cognome = prompt("qual è il tuo cognome?")
+var colore = prompt("qual è il tuo colore preferito?")
+document.getElementById('pass').innerHTML = nome + cognome + colore + 20;
